aed24d68505b58ce34a3f8a422fb7fbab98f460c
Ida Delphine <idadelm@gmail.com>
2024-11-11 15:58:33 +0100

    update : fix bridges ui

--- app/src/main/java/com/example/sw0b_001/Bridges/BridgesSubmitCodeFragment.kt
+++ app/src/main/java/com/example/sw0b_001/Bridges/BridgesSubmitCodeFragment.kt
@@ -5,15 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.sw0b_001.R
+import com.google.android.material.card.MaterialCardView
 
-class BridgesSubmitCodeFragment(private val onSuccessRunnable: Runnable? = null) : Fragment(R.layout.fragment_bridges_auth_code) {
+class BridgesSubmitCodeFragment() : Fragment(R.layout.fragment_bridges_auth_code) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val infoBoxTextView = view.findViewById<TextView>(R.id.telegram_info_text_view)
         infoBoxTextView.text = "Once you send an authentication request via SMS, you will receive a message containing the code. Copy the entire SMS without adding or removing anything from it and paste it here"
+
+        val infoBoxCardView = view.findViewById<MaterialCardView>(R.id.telegram_info_box)
+        val infoBoxColor = ContextCompat.getColor(requireContext(), R.color.md_theme_primaryContainer)
+        infoBoxCardView.setCardBackgroundColor(infoBoxColor)
     }
 }
