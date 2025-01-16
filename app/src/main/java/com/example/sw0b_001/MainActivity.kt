4dfe494c40367735d409ef2cc296c88c4622728a
sherlock <wisdomnji@gmail.com>
2025-01-16 21:22:26 +0100

    - update: created structure

--- app/src/main/java/com/example/sw0b_001/MainActivity.kt
+++ app/src/main/java/com/example/sw0b_001/MainActivity.kt
@@ -1,25 +1,31 @@
 package com.example.sw0b_001
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.fragment.app.viewModels
 import com.example.compose.AppTheme
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.sw0b_001.Models.Messages.MessagesViewModel
 import kotlinx.serialization.Serializable
+import kotlin.getValue
 
 @Serializable
-object HomeScreen
+object RecentScreen
+
+@Serializable
+object GatewayClientScreen
 
 class MainActivity : AppCompatActivity() {
     lateinit var navController: NavHostController
+    val messagesViewModel: MessagesViewModel = MessagesViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +41,9 @@ class MainActivity : AppCompatActivity() {
                     NavHost(
                         modifier = Modifier,
                         navController = navController,
-                        startDestination = HomeScreen,
+                        startDestination = RecentScreen,
                     ) {
-                        composable<HomeScreen> {
+                        composable<RecentScreen> {
 
                         }
                     }
